9bb7b00bf63a8e0f1ce42ff1233bd19a85927ba7
Moritz Flaschel <62348310+mflaschel@users.noreply.github.com>
2025-08-19 13:50:08 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     author="Moritz Flaschel",
     author_email="moritz.flaschel@fau.de",
     url="https://github.com/Material-Fingerprinting/material-fingerprinting",
+    description="A shortcut to material model discovery without solving optimization problems.",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     packages=find_packages(),
